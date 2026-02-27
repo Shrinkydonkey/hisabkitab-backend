c64627ac16c9e6873ac80d2e12c253bfddcc6a20
Piyush-Samal <piyush.24scse1180167@galgotiasuniversity.ac.in>
2026-02-27 10:02:30 +0530

    Fixed port for render

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
